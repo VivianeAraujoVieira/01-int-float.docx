fc0d885343b1cf06363281fcec60d16343ee655f
ViviAraujo <vyviaraujo@hotmail.com.br>
2022-03-25 16:22:11 -0300

    exercicio_1.2
    área de um triangulo

---
+++ exercicio_1.2.py
@@ -0,0 +1,11 @@
+
+'''Faça um programa que receba a base e altura de um triângulo e imprima a área dele'''
+
+from re import A
+
+
+base = float(input("Informe qual o valor de sua base: "))
+alt = float(input("Informe à altura: "))
+
+area = (base * alt)/2
+print(f"A área do seu triangulo será: {area}")
