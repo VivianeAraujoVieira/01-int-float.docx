28eef017512e3f7092a70092454b619fedef585e
ViviAraujo <vyviaraujo@hotmail.com.br>
2022-03-27 20:16:43 -0300

    corrigindo erro

--- exercicio_1.4.py
+++ exercicio_1.4.py
@@ -9,12 +9,21 @@ alt = float(input("Agora informe a sua altura:  "))
 imc = peso / alt**2
 
 if (imc<=18.5):
-    print(f" {nome} seu imc foi {imc:.2f} você esta abaixo do peso. ")
+    print(f"{nome}, seu imc é {imc:.2f} você esta abaixo do peso. ")   
+    print(" Valores de referência:\n imc<=18.5 = abaixo do peso\nimc 19 à 24.9 = Normal \nimc 25 à 29.9 = Acima do peso \nimc>=30 = Obesidade \nimc>=40 = Obesidade morbida.")
+
 if (imc>=19) and (imc<24.9):
         print(f"Parabéns {nome}, seu imc é {imc:.2f}, você esta dentro do peso ideal. ")
+        print(" Valores de referência: imc<=18.5 = abaixo do peso\n imc 19 à 24.9 = Normal \nimc 25 à 29.9 = Acima do peso \nimc>=30 = Obesidade \nimc>=40 = Obesidade morbida.")
+
 if (imc>=25) and (imc<=29.9):
     print(f"{nome}, seu imc é {imc:.2f}, esta um pouco acima do peso, cuide-se! ")
+    print(" Valores de referência: imc<=18.5 = abaixo do peso\n imc 19 à 24.9 = Normal \nimc 25 à 29.9 = Acima do peso \nimc>=30 = Obesidade \nimc>=40 = Obesidade morbida.")
+
 if (imc>=30):
     print (f"{nome}, seu imc é {imc:.2f}, você esta obeso! ")
+    print(" Valores de referência: imc<=18.5 = abaixo do peso\n imc 19 à 24.9 = Normal \nimc 25 à 29.9 = Acima do peso \nimc>=30 = Obesidade \nimc>=40 = Obesidade morbida.")
+
 if (imc>=40):
     print(f"{nome}, seu imc é {imc}, você esta com obesidade morbida! ")
+    print(" Valores de referência: imc<=18.5 = abaixo do peso\n imc 19 à 24.9 = Normal \nimc 25 à 29.9 = Acima do peso \nimc>=30 = Obesidade \nimc>=40 = Obesidade morbida.")
